d194e9051856fafa510b679307c697c80a32cb68
Alexis250 <106118006+Alexis250@users.noreply.github.com>
2022-05-23 23:55:44 +0200

    Update db.php

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$dsn = 'mysql:host=localhost;dbname=work';
+$username = 'root';
+$password = '';
+$options = [];
+try {
+$connection = new PDO($dsn, $username, $password, $options);
+} catch(PDOException $e) {
+
+}
+?>
